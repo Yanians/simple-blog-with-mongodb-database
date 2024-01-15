40cd67d5f9b25e40b1d98e6e289234c3a8d34cec
Yanians <paylastres@gmail.com>
2024-01-15 21:54:15 +0800

    final pace

---
+++ build/precache-manifest.cbd77ed858065ffa42f0c7a218a85ae0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "00e509e302bfcbb5e941",
+    "url": "/static/css/main.343440d3.chunk.css"
+  },
+  {
+    "revision": "00e509e302bfcbb5e941",
+    "url": "/static/js/main.d2a0d3ba.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "24cff1aef95d18a90b36",
+    "url": "/static/js/2.60900d06.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "24c51dfb154b9afe34dc306465a53be4",
+    "url": "/index.html"
+  }
+];
